0265a78455e42dabfb775be82024bf1792d492e7
Kye Choong <kyecdesign@gmail.com>
2024-05-07 15:47:32 +1000

    minor tweaks

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -18,6 +18,7 @@ const Works = () => {
                         <img src="assets/images/CaseStudy1/artjammers_thumbnail.jpg" alt="artjammers" />
                         <div>
                             <h3>ArtJammers</h3>
+                            <h4>PERSONAL PROJECT</h4>
                             <h4>App, Art, Social</h4>
                             <p>An app that allows hobbyist artists in Tokyo to make meaningful connections through art</p>
                         </div>
@@ -27,7 +28,7 @@ const Works = () => {
                     <Link to="/case-study-2" onClick={scrollToTop}><img src="assets/images/CaseStudy2/projectthumbnail2.png" alt="roche" />
                         <div>
                             <h3>Roche</h3>
-                            <h4>Client work</h4>
+                            <h4>CLIENT WORK</h4>
                             <h4>Website, Medical, B2B</h4>
                             <p>Redesign of the Roche website to help drive more traffic and reorganized the IA for ease of use.</p>
                         </div>
@@ -37,7 +38,7 @@ const Works = () => {
                     <Link to="/case-study-3" onClick={scrollToTop}><img src="assets/images/CaseStudy3/projectthumbnail3.png" alt="avertrolite" />
                         <div>
                             <h3>Avertro Lite</h3>
-                            <h4>Client work</h4>
+                            <h4>CLIENT WORK</h4>
                             <h4>SaaS, Cybersecurity, B2B</h4>
                             <p>A simplified dashboard tailored for a cybersecurity SaaS product, specifically catering to SME's</p>
                         </div>
